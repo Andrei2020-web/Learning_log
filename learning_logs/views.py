48ad50a30b7c41c75123061a32c73ddee15b8c5e
Asus <Asus@Asus>
2021-06-26 12:36:11 +0300

    Добавлена проверка входа пользователя в систему и возможность работы пользователя только со своими темами\записями.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from django.template.context_processors import request
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 
 # Create your views here.
@@ -9,21 +13,31 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Выводит список тем."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все её записи."""
     topic = Topic.objects.get(id=topic_id)
+    """Проверка того, что тема принадлежит текущему пользователю."""
+    check_topic_owner(request, topic)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
+        raise Http404
+
+
+@login_required
 def new_topic(request):
     """Определяет новую тему."""
     if request.method != 'POST':
@@ -33,7 +47,9 @@ def new_topic(request):
         """Отправлены данные POST, обрабатывать данные."""
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     """Вывести пустую или недействительную форму."""
@@ -41,6 +57,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запист по конкретной теме."""
     topic = Topic.objects.get(id=topic_id)
@@ -51,6 +68,7 @@ def new_entry(request, topic_id):
         # Отправлены данные POST, обработать данные
         form = EntryForm(data=request.POST)
         if form.is_valid():
+            check_topic_owner(request, topic)
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
@@ -61,10 +79,12 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует текущую запись."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Форма заполняется данными из БД.
